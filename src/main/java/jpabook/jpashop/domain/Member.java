fd5afd7d8474c1a3ec2dc7fd51a403cdacf76a8a
Ysw <202021079@sangmyung.kr>
2024-08-31 23:51:24 +0900

    회원, 주소, 주문 엔티티 생성

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,5 +1,31 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded //Address 엔티티에 @Embeddable가 있으면 따로 설정 안해도 되지만 표시를 위해 해도 상관없음.
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
 
 }
